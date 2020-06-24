6ffa0790dbfad6c1475afbb8086550a4f828ad78
Kortnie Heidel <kortnie.evans@gmail.com>
2020-06-24 19:12:09 -0400

    Updates

---
+++ precache-manifest.cd8a037a7441d613d835ae114350c842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0beca85d0667364094ac4a5effee12",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f7421b7ae99c8422ee7b",
+    "url": "/clicky-game/static/css/main.85c2351f.chunk.css"
+  },
+  {
+    "revision": "fa16647add3b33dfa690",
+    "url": "/clicky-game/static/js/2.680c17b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.680c17b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7421b7ae99c8422ee7b",
+    "url": "/clicky-game/static/js/main.df31353f.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
